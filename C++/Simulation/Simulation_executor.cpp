9cf3cd4f1cc9440ba93def66fbddf69e15c4d3dd
Daniel Schenk <daniel.r.schenk@gmail.com>
2024-09-21 17:19:53 +0200

    Correct delays

--- C++/Simulation/Simulation_executor.cpp
+++ C++/Simulation/Simulation_executor.cpp
@@ -3,27 +3,27 @@
 #include <string>
 #include <future>
 #include "Simulation_endpoint.cpp"
+#include <vector>
 
 
 using namespace std;
 
-    int* create_delay_array(int total_millis, int total_calls) {
-        int delays[total_calls];
+    vector<int> create_delay_array(int total_millis, int total_calls) {
+        vector<int> delays(total_calls, 1);
         if (total_millis < total_calls) {
             cout << "Error: millis is lower than calls" << endl;
             return delays;
         }
         cout << "network start" << endl;
-    
 
-        //Make delay array where every delay is at least 1 millis
-        for  (int i = 0; i < sizeof(delays)/sizeof(int); i++) {
-            delays[i] = 1;
-        }
+        srand(time(0));
         int total_millis_left = total_millis - total_calls;
         while (total_millis_left > 0) {
-            delays[rand()%sizeof(delays)/sizeof(int)]++;
+            delays[rand() % total_calls]++;
             total_millis_left--;
+        }
+        for(int i = 0; i < total_calls; i++) {
+            cout << delays[i] << " ";
         }
         return delays;
     }
@@ -31,13 +31,13 @@ using namespace std;
 int main(int argc, char* argv[])  
 {  
     Endpoint ep;
-    int *delays = create_delay_array(10000, 1000);
-    cout << &delays[5] << endl;
+    vector<int> delays = create_delay_array(10000, 1000);
+    cout << delays[1] << endl;
     
     // std::future<int> test = std::async(std::launch::async, ep.network, 10000, 1000);
     // std::future<int> test2 = std::async(std::launch::async, ep.disk);
     // std::cout << "Result: " << test.get() << std::endl;
     // std::cout << "Result2: " << test2.get() << std::endl;
-
+    return 0;
 }
 
